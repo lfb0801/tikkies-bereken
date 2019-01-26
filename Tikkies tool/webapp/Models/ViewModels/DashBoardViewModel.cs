6c290aed362372196fb2ad62e17ee1379dd1c91c
Robin <>
2019-01-26 12:49:31 +0100

    UI begonnen

---
+++ Tikkies tool/webapp/Models/ViewModels/DashBoardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapp.Models.ViewModels
+{
+    public class DashBoardViewModel
+    {
+        public string Naam { get; private set; }
+
+        public DashBoardViewModel(string naam)
+        {
+            Naam = naam;
+        }
+    }
+}
