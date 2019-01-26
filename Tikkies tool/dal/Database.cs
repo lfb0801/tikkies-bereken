6d471af63028e7e9dd60a04eb1cced1afa4247a0
joris <jorisvanroy@onsmail.nl>
2019-01-26 12:32:19 +0100

    Created Dal layer and added start of database

---
+++ Tikkies tool/dal/Database.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace dal
+{
+    class Database
+    {
+        private static Database instance = new Database("studmysql01.fhict.local","dbi379275","dbi379275", "OuluTrekkers");
+        private static MySqlConnection conn;
+
+        public Database(string server, string db, string user, string pass)
+        {
+            conn = new MySqlConnection($"Server={server};Database={db};Uid={user};Pwd={pass};");
+        }
+
+        public static Database GetInstance()
+        {
+            return instance;
+        }
+
+
+        public DataTable Query(string query)
+        {
+            Connect();
+            var dt = new DataTable();
+            var cmd = new MySqlCommand(query, conn);
+            var reader = cmd.ExecuteReader();
+            dt.Load(reader);
+            Disconnect();
+            return dt;
+        }
+
+        public void NonQuery(string query)
+        {
+            Connect();
+            var cmd = new MySqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+            Disconnect();
+        }
+
+        private void Connect()
+        {
+            conn.Open();
+        }
+
+        private void Disconnect()
+        {
+            conn.Close();
+        }
+    }
+
+}
